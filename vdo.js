58d1b2e879c2f2b0ed8c34a5bf533af531504161
Josh <thekitichai1545@gmail.com>
2024-11-05 00:35:21 +0700

    อพโหลดไฟลทงหมด

--- vdo.js
+++ vdo.js
@@ -1,13 +1,13 @@
 window.onload = function() {
-    // หยุดวิดีโอและลบปุ่มควบคุมสำหรับหน้าจอมือถือ
     if (window.innerWidth <= 768) {
         const video2 = document.querySelector('.background-video2');
         const video3 = document.querySelector('.background-video3');
 
-        video2.pause(); // หยุดเล่นวิดีโอสำหรับ blog2
-        video2.removeAttribute('controls'); // ลบปุ่มควบคุม
+        // หยุดวิดีโอและลบปุ่มควบคุม
+        video2.pause();
+        video2.removeAttribute('controls');
 
-        video3.pause(); // หยุดเล่นวิดีโอสำหรับ blog3
-        video3.removeAttribute('controls'); // ลบปุ่มควบคุม
+        video3.pause();
+        video3.removeAttribute('controls');
     }
 };
